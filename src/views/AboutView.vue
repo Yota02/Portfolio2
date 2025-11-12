1f083c80005802ff665c9c7699e8a6f6e5ffb613
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2025-11-12 23:14:16 +0100

    fix: ais -> ai

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -38,9 +38,9 @@
         <div class="commandements-section card" role="region" aria-labelledby="commandements-title">
           <h2 id="commandements-title"><i class="fas fa-scroll" aria-hidden="true"></i> Mes 3 commandements</h2>
           <ul class="commandements-list">
-            <li>"Je ne passerais pas un jour sans apprendre au moins 1 chose"</li>
-            <li>"Je poserais toujours des questions pour comprendre le monde"</li>
-            <li>"J'explorerais de nouvelles idées sans crainte de l'échec"</li>
+            <li>"Je ne passerai pas un jour sans apprendre au moins 1 chose"</li>
+            <li>"Je poserai toujours des questions pour comprendre le monde"</li>
+            <li>"J'explorerai de nouvelles idées sans crainte de l'échec"</li>
           </ul>
         </div>
 
