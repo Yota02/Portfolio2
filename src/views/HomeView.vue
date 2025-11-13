014356e2b2fe9c4ea6ce872f8812397852127fc1
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2025-11-13 08:12:45 +0100

    feat: update HomeView.vue to reflect student status and career goals

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,8 +24,7 @@
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
               </svg>
             </a>
-            <!-- Add this link with PDF icon for downloading cv.pdf -->
-            <a href="/pdf/cv.pdf" download class="social-icon" title="mon cv">
+            <a href="../../public/pdf/cv.pdf" download class="social-icon" title="mon cv">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm2 16h-4v-4h-4l6-6v4h4v6z"/>
               </svg>
